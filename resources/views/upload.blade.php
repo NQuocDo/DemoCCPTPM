c6c7d4e7b315cd7d90a8bc401f6363935dcde856
do <quocdo113cscd@gmail.com>
2024-11-01 07:49:22 +0700

    them noi dung

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -1,5 +1,7 @@
 <form method="POST" action="{{ route('do-upload') }}" enctype="multipart/form-data">
     @csrf
+    <p>Hãy nhập hình ảnh vào đây </p>
     <input type="file" multiple name="hinh_anh" />
     <button type="submit">Upload</button>
+
 </form>
