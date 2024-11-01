2fece42ff91eca2861a7524750cd2832b3645aae
do <quocdo113cscd@gmail.com>
2024-11-01 07:51:05 +0700

    XOA noi dung

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -1,6 +1,5 @@
 <form method="POST" action="{{ route('do-upload') }}" enctype="multipart/form-data">
     @csrf
-    <p>Hãy nhập hình ảnh </p>
     <input type="file" multiple name="hinh_anh" />
     <button type="submit">Upload</button>
 
